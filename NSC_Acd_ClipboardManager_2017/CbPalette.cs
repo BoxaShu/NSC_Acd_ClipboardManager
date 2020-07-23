00dcf35949b22979279a450f1f0dff13b2241e6a
Владимир Шульжицкий <boxa.shu@gmail.com>
2020-07-23 09:22:34 +0300

    Осталось реализовать класс CbPalette и протестировать DemandLoading

--- NSC_Acd_ClipboardManager_2017/CbPalette.cs
+++ NSC_Acd_ClipboardManager_2017/CbPalette.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 using AcApp = Autodesk.AutoCAD.ApplicationServices.Application;
 using System.Windows.Forms;
 
-
+/// <summary>
+/// https://through-the-interface.typepad.com/through_the_interface/2009/09/clipboard-manager-octobers-adn-plugin-of-the-month-now-live-on-autodesk-labs.html
+/// </summary>
 namespace NSC_Acd_ClipboardManager
 {
   public class CbPalette
